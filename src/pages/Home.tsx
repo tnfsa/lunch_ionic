780b3bff5af278736e09ac39c4c08ce33f81598c
milliax <wei@sivir.pw>
2022-02-12 16:09:41 +0000

    home and search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,25 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonCard,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonItem,
+    IonImg
+} from '@ionic/react';
+
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import 'swiper/css/bundle'
+
+// import { useEffect } from 'react';
 
 const Home: React.FC = () => {
+    SwiperCore.use([Autoplay]);
     return (
         <IonPage>
             <IonHeader>
@@ -14,10 +33,61 @@ const Home: React.FC = () => {
                         <IonTitle size="large">美廣點餐系統</IonTitle>
                     </IonToolbar>
                 </IonHeader>
-                
+                <Carousel />
             </IonContent>
         </IonPage>
     );
 };
+
+const Carousel: React.FC = () => {
+    return (
+        <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={10}
+            speed={400}
+            loop={true}
+            autoplay={{ delay: 5000 }}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSlideChange={() => { console.log('slide change') }}
+            onSwiper={swiper => { console.log(swiper) }}>
+
+            {carousel_info.map((data: { src: string; text: string; brand: string }, index: number) => (
+                <SwiperSlide key={index}>
+                    <IonCard className="px-5 md:px-10">
+                        <IonItem>
+                            <IonImg src={data.src} className="mx-auto mb-auto object-cover md:h-80 md:w-96 h-44 w-52" />
+                            <div className="flex flex-col space-y-5 md:w-40 w-28">
+                                <IonCardTitle>{data.brand}</IonCardTitle>
+                                <IonCardSubtitle>{data.text}</IonCardSubtitle>
+                            </div>
+                        </IonItem>
+                    </IonCard>
+                </SwiperSlide>
+            ))}
+
+        </Swiper>
+    )
+}
+
+const carousel_info = [
+    {
+        src: "https://i.imgur.com/MrGY5EL.jpeg",
+        text: "Windows XP!!",
+        brand: "Imger"
+    },
+    {
+        src: "https://i.pinimg.com/originals/e0/0e/23/e00e23a03f611656eae3b5bb0c29da37.gif",
+        text: "Hashimoto Kanna!",
+        brand: "Pinterest"
+    },
+    {
+        src: "https://i.pinimg.com/564x/bc/5a/c0/bc5ac023df850aa9ace659c66130ac61.jpg",
+        text: "Kanna",
+        brand: "Pinterest"
+    }
+]
 
 export default Home;
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,6 +1,76 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonSearchbar,
+    IonCard,
+    IonCardContent,
+    IonCardSubtitle,
+    IonCardHeader,
+    IonCardTitle,
+    IonProgressBar,
+    IonImg
+} from '@ionic/react';
+
+import { useState, useEffect } from 'react';
 
 const Search: React.FC = () => {
+    const [searchParam, setSearchParam] = useState("");
+    const [view, setView] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [result, setResult] = useState([{ price: 100, description: "bad", name: "Helloween", store_id: "best_store", id: "whoami",image: "haha" }]);
+    const [storeData, setStoreData] = useState({})
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_ENDPOINT}/stores`, {
+            method: "GET",
+            headers: { Accept: "application/json" }
+        }).then(response => {
+            return response.json()
+        }).then(res => {
+            let List = {};
+            res.map((item: { id: string; name: string }) => {
+                List = Object.assign(List, { [item.id]: item.name })
+                return "garbage"
+            })
+            setStoreData(List)
+        })
+    }, [])
+
+    const search = async () => {
+        console.log("Searching")
+        setLoading(true)
+        const url = `${process.env.REACT_APP_API_ENDPOINT}/products/query`
+        console.log(url)
+        const res = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                term: searchParam
+            })
+        });
+        const fetched = await res.json();
+        console.log(fetched)
+        const blackList = ["fea67b81-5b53-4b60-a021-525c41989174", "bc6e15a9-7c15-4d6e-b97a-330ff1b350a3"]
+        let ans = []
+        for (let i = 0;i < fetched.length; ++i) {
+            console.log(blackList[0] === fetched[i]['store_id'])
+            console.log(blackList[1] === fetched[i]['store_id'])
+
+            if (blackList[1] === fetched[i]['store_id']) {
+                // do nothing
+            }else if(blackList[0] === fetched[i]['store_id']){
+                // do nothing
+            }else{
+                ans.push(fetched[i])
+            }
+        }
+        setResult(ans);
+        setLoading(false);
+        setView(true);
+    }
+
     return (
         <IonPage>
             <IonHeader>
@@ -14,10 +84,54 @@ const Search: React.FC = () => {
                         <IonTitle size="large">美廣點餐系統</IonTitle>
                     </IonToolbar>
                 </IonHeader>
-                
+                <IonHeader translucent>
+                    <IonToolbar>
+                        <IonTitle className="text-center">搜尋商品</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonProgressBar type="indeterminate" hidden={!loading} /><br />
+                <form onSubmit={(e) => { e.preventDefault(); search() }}>
+                    <IonSearchbar value={searchParam}
+                        onIonChange={e => setSearchParam(e.detail.value!)}
+                        showCancelButton="focus"
+                        inputMode="search" />
+                </form>
+                <div hidden={!view}>
+                    {result.map((data: {price: number;image: string;description: string;name:string;store_id:string;id: string;}) => (
+                        <IonCard key={data.id} className="flex flex-row h-32">
+                            <div className="mx-auto">
+                                <IonImg src={data.image} className="object-cover h-32" />
+                            </div>
+                            <div>
+                                <IonCardHeader>
+                                    <IonCardSubtitle>{store_id2name(data.store_id, storeData)}</IonCardSubtitle>
+                                    <IonCardTitle>{data.name}</IonCardTitle>
+                                </IonCardHeader>
+                                <IonCardContent>
+                                    {data.description}
+                                </IonCardContent>
+                            </div>
+                        </IonCard>
+                    ))}
+                </div>
             </IonContent>
         </IonPage>
     );
 };
+
+
+
+type tlist = {
+    [key: string]: string
+}
+
+function store_id2name(storeID: string, list: tlist) {
+    if (storeID in list) {
+        return list[storeID];
+    }
+    return "undefined"
+}
+
+
 
 export default Search;
